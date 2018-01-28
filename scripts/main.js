97939c2b9f7fe751f3bbac2712f05742d8ed54c4
sukai <35886709+silver8177@users.noreply.github.com>
2018-01-28 16:02:04 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading=document.querySelector('h1');
+myHeading.innerHTML='Hello world!';
+
+/*Evertyhing is a comment.*/
+//This is a comment
+
+var myVariable;
+myVariable='Bob'
+myVariable;
