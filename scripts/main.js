a6e0ac289ae5f5489996df408da8bcc0fe86e144
sukai <35886709+silver8177@users.noreply.github.com>
2018-01-30 15:51:32 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,33 @@
-var myHeading=document.querySelector('h1');
-myHeading.innerHTML='Hello world!';
+// Image switcher code
+var myImage = document.querySelector('img');
 
-/*Evertyhing is a comment.*/
-//This is a comment
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/Firefox_Logo.svg.png') {
+		myImage.setAttribute('src','images/red-fox.jpg');
+	} else {
+		myImage.setAttribute('src','images/Firefox_Logo.svg.png');
+	}
+}
 
-var myVariable;
-myVariable='Bob'
-myVariable;
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name',myName);
+	myHeading.innerHTML = 'Mozilla is cool,' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Mozilla is cool,' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
